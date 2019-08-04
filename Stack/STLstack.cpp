809920dc0749322f330ce2ddfa4a15ca15cf886a
maniSHarma7575 <39980734+maniSHarma7575@users.noreply.github.com>
2019-08-04 11:04:01 +0530

    Create STLstack.cpp

---
+++ Stack/STLstack.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+void showstack(stack<int> s)
+{
+	while(!s.empty())
+	{
+		cout<<'\t' <<s.top();
+		s.pop();
+	}
+	cout<<" \n";
+}
+int main() {
+	stack<int> s;
+	s.push(10);
+	s.push(30);
+	s.push(20);
+	s.push(5);
+	s.push(1);
+	cout<<"The stack is :";
+	showstack(s);
+	
+	cout<<"\n size(): " << s.size();
+	cout<<"\n s.top(): " <<s.top();
+	cout<<"\n s.pop():";
+	s.pop();
+	showstack(s);
+	return 0;
+}
